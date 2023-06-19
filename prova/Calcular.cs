fca2a0bbdbf2f09e038ed1fe3d1bdcc192e7f583
Paulo Rogério <secundariarmg2007@gmail.com>
2023-06-19 18:26:36 -0300

    ultimo commit

--- prova/Calcular.cs
+++ prova/Calcular.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace prova
+﻿public class Calcular 
 {
-    internal class Calcular
+    Calcular c = new Calcular();
+
+    public void Servicos(string formatacao, string tipo_pagamento)
     {
+        /*infelizmente estava nervoso e me deu um branco porque nao consegui estudar meus codigos antigos porque meu computador da escola foi formatado
+        //espero que no proximo bimestre e na recuperaçao eu va melhor
     }
+
 }
--- prova/Menu.cs
+++ prova/Menu.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/*acabei ficando muito nervoso por nao poder usar o meu computador do laboratorio e isso me fez dar um branco em tudo, desculpe por isso*/
+
 namespace prova
 {
     public partial class Menu : Form
@@ -15,6 +17,7 @@ namespace prova
         public Menu()
         {
             InitializeComponent();
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -24,7 +27,7 @@ namespace prova
 
         private void salvar_Click(object sender, EventArgs e)
         {
-            MessageBox.Show(computador.Text + cliente.Text + servico.Text + tipo_pagamento);
+            if(servico.)
         }
 
         private void computador_TextChanged(object sender, EventArgs e)
@@ -45,6 +48,11 @@ namespace prova
         private void tipo_pagamento_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
